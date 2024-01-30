0846463728316e3cff8f933dcea85b3a351c5786
Ankit Mishra <ankit.mishra@paavu.com>
2024-01-30 13:44:36 +0530

    change

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/switchTab/SwitchTabs'
 
 const Trending = () => {
+
+    const onTabsChange = () => { }
+
     return (
         <div className="carouselSection">
             <ContentWrapper>
                 <h1>Trending</h1>
+                <SwitchTabs data={["Day", "Week"]} onTabsChange={onTabsChange} />
             </ContentWrapper>
         </div>
     )
