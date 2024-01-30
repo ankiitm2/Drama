e6a0a7ba68d3f19019a70795f5afb0a7ef46d196
Ankit Mishra <ankit.mishra@paavu.com>
2024-01-30 18:47:03 +0530

    change

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchTab/SwitchTabs'
+import useFetch from "../../../hooks/useFetch"
+import Carousel from '../../../components/carousel/Carousel'
 
 const Trending = () => {
+    const [endPoint, setEndPoint] = useState("day");
 
-    const onTabsChange = () => { }
+    const { data, loading } = useFetch(`/trending/movie/${endPoint}`)
+
+    const onTabsChange = (tab) => {
+        setEndPoint(tab === "Day" ? "day" : "week");
+    }
 
     return (
         <div className="carouselSection">
@@ -12,6 +19,7 @@ const Trending = () => {
                 <h1>Trending</h1>
                 <SwitchTabs data={["Day", "Week"]} onTabsChange={onTabsChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     )
 }
