536905c31e183433d6800511ad91df1c3f8ee0dc
Ankit Mishra <ankit.mishra@paavu.com>
2024-01-31 15:47:29 +0530

    change

---
+++ src/pages/home/popular/Popular.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/switchTab/SwitchTabs'
+import useFetch from "../../../hooks/useFetch"
+import Carousel from '../../../components/carousel/Carousel'
+
+const Popular = () => {
+    const [endPoint, setEndPoint] = useState("movie");
+
+    const { data, loading } = useFetch(`/${endPoint}/popular`)
+
+    const onTabsChange = (tab) => {
+        setEndPoint(tab === "Movie" ? "movie" : "tv");
+    }
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <h1>Popular</h1>
+                <SwitchTabs data={["Movie", "TV Shows"]} onTabsChange={onTabsChange} />
+            </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
+        </div>
+    )
+}
+
+export default Popular
