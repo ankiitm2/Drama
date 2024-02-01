2297992ae6328fe64bb6e5968ede97ce76e41ecf
Ankit Mishra <ankit.mishra@paavu.com>
2024-02-01 11:51:05 +0530

    second

---
+++ src/pages/home/topRated/TopRated.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import SwitchTabs from '../../../components/switchTab/SwitchTabs'
+import useFetch from "../../../hooks/useFetch"
+import Carousel from '../../../components/carousel/Carousel'
+
+const TopRated = () => {
+    const [endPoint, setEndPoint] = useState("movie");
+
+    const { data, loading } = useFetch(`/${endPoint}/top_rated`)
+
+    const onTabsChange = (tab) => {
+        setEndPoint(tab === "Movie" ? "movie" : "tv");
+    }
+
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <h1>TopRated</h1>
+                <SwitchTabs data={["Movie", "TV Shows"]} onTabsChange={onTabsChange} />
+            </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} endPoint={endPoint} />
+        </div>
+    )
+}
+
+export default TopRated
