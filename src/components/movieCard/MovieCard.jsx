2b3e7907d4f5532cf293bcf97459e73fa2e536c7
Ankit Mishra <ankit.mishra@paavu.com>
2024-02-05 11:36:14 +0530

    second

---
+++ src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.scss";
+
+const MovieCard = () => {
+    return (
+        <div>MovieCard</div>
+    )
+}
+
+export default MovieCard
--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -6,11 +6,60 @@ import "./style.scss"
 
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
-
+import MovieCard from "../../components/movieCard/MovieCard"
+import Spinner from "../../components/spinner/Spinner"
+import noResults from "../../assets/no-results.png"
 
 const SearchResults = () => {
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const { query } = useParams();
+
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res) => {
+            setData(res)
+            setPageNum((prev) => prev + 1)
+            setLoading(false);
+        })
+    }
+
+    const fetchNextPageData = () => {
+        fetchDataFromApi(`/search/multi?query=${query}/&page=${pageNum}`).then((res) => {
+            if (data?.results) {
+                setData({
+                    ...data, results: [...data?.results, ...res.results],
+                })
+            } else { setData(res) }
+            setPageNum((prev) => prev + 1)
+        })
+    }
+
+    useEffect(() => {
+        fetchInitialData()
+    }, [query])
+
     return (
-        <div>SearchResults</div>
+        <div className="searchResultsPage">
+            {loading && <Spinner initial={true} />}
+            {!loading && (
+                <ContentWrapper>
+                    {data.results.length > 0 ? (
+                        <>
+                            <div className="pageTitle">
+                                {`Search ${data.total_page > 1 ? "results" : "result"} of '${query}'`}
+                            </div>
+                        </>
+                    ) : (
+                        <span class="resultNotFound">
+                            Sorry, No results Found
+                        </span>
+                    )}
+                </ContentWrapper>
+            )
+            }
+        </div >
     )
 }
 
